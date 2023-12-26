710cf0b2b2877ec35786c5d1ce1a065fd4ac310c
Mufil Rahman A <ramanizum@gmail.com>
2023-12-26 17:51:30 +0530

    working with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import './App.css'
+
+import Header from './components/Header'
 function App() {
   let author = "Mufil Rahman"
   return (
   <div>
-    <h1 className='heading'>my first application</h1>
+    <Header/>
     <br/>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sed earum veritatis, labore itaque esse.</p>
     <p>Author <span>{author}</span></p>
-    <Footer/>
   </div>
   );
 }
 
 export default App;
 
-function Footer(){
-  return(
-    <div>
-      <p>I am footer from a function</p>
-    </div>
-  )
-}
+
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import '../App'
+function Header(){
+    return(
+      <div>
+        <h1 className='heading'>I am header components</h1>
+      </div>
+    )
+  }
+  export default Header;
