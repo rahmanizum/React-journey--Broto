fa12eb3f7ebc67c8a9eb92bbd47cd8e1d2378bcd
Mufil Rahman A <ramanizum@gmail.com>
2023-12-27 15:42:58 +0530

    mounting,unamounting,updating,useeffect

--- firstapp-1/src/App.js
+++ firstapp-1/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Card from './components/Card';
 
 function App() {
@@ -17,13 +17,23 @@ function App() {
     bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ad culpa rem rerum aliquam tempora!"
   }]
   const [state, setState] = useState(true)
+  const[count,SetCount] = useState(0);
+  useEffect(()=>{
+    console.log("mounting...."+count);
+    return ()=>{
+      console.log('cleanup'+count);
+    }
+  },[count])
 
   return (
     <div className="text-center p-3">
       <h1>Hello World!</h1>
       <div className='btn-group text-center pb-3'>
+        <button onClick={()=>SetCount(count+1)} className='btn btn-success'>Incriment</button>
         <button onClick={() => setState(!state)} className='btn btn-primary '> hide/show</button>
+        <button onClick={()=>SetCount(count-1)} className='btn btn-warning'>Decriment</button>
       </div>
+      <p className='lead'>The count : {count}</p>
       <div className='d-flex justify-content-around'>
         {objArr.map((obj, index) => {
           return state && <Card key={index}{...obj} /> 
